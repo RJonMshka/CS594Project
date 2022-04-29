32ad8c021e735be11ff50cf5f51e39be1df0803f
Kumar <rkumar40@uic.edu>
2022-04-29 11:11:05 -0500

    fixed technical indicators issue

--- Ensemble/preprocessing.py
+++ Ensemble/preprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from stockstats import StockDataFrame as Sdf
 from config import *
 
 def load_dataset(*, file_name: str) -> pd.DataFrame:
@@ -30,14 +31,54 @@ def preprocess_data():
     df = df[df.datadate>=20090000]
     # calcualte adjusted price
     df_preprocess = calculate_price(df)
-    df_preprocess.fillna(method='bfill',inplace=True)
-    return df_preprocess
+    df_final=add_technical_indicator(df_preprocess)
+    # fill the missing values at the beginning
+    df_final.fillna(method='bfill',inplace=True)
+    return df_final
 
 def add_turbulence(df):
     turbulence_index = calculate_turbulence(df)
     df = df.merge(turbulence_index, on='datadate')
     df = df.sort_values(['datadate','tic']).reset_index(drop=True)
     return df
+
+def add_technical_indicator(df):
+    stock = Sdf.retype(df.copy())
+
+    stock['close'] = stock['adjcp']
+    unique_ticker = stock.tic.unique()
+
+    macd = pd.DataFrame()
+    rsi = pd.DataFrame()
+    cci = pd.DataFrame()
+    dx = pd.DataFrame()
+    for i in range(len(unique_ticker)):
+        ## macd
+        temp_macd = stock[stock.tic == unique_ticker[i]]['macd']
+        temp_macd = pd.DataFrame(temp_macd)
+        macd = macd.append(temp_macd, ignore_index=True)
+        ## rsi
+        temp_rsi = stock[stock.tic == unique_ticker[i]]['rsi_30']
+        temp_rsi = pd.DataFrame(temp_rsi)
+        rsi = rsi.append(temp_rsi, ignore_index=True)
+        ## cci
+        temp_cci = stock[stock.tic == unique_ticker[i]]['cci_30']
+        temp_cci = pd.DataFrame(temp_cci)
+        cci = cci.append(temp_cci, ignore_index=True)
+        ## adx
+        temp_dx = stock[stock.tic == unique_ticker[i]]['dx_30']
+        temp_dx = pd.DataFrame(temp_dx)
+        dx = dx.append(temp_dx, ignore_index=True)
+
+
+    df['macd'] = macd
+    df['rsi'] = rsi
+    df['cci'] = cci
+    df['adx'] = dx
+
+    return df
+
+
 
 
 
